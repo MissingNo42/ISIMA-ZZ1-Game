1c8632f15ed897610c6a8a76acfabdc6651e32bf
Vurremt <72647204+Vurremt@users.noreply.github.com>
2023-07-03 17:45:01 +0200

    Mise a jour champs direction + distance avec proies / predateurs / alliés et mise à jour de la matrice

--- field.c
+++ field.c
@@ -2,11 +2,10 @@
 #include <stdlib.h>
 #include <math.h>
 
+#incclude "field.h"
 #include "population.h"
 
-#define SIZEMAP 15
-#define PERCENTNEAR 0.2
-#define PERCENTMEDIUM 0.5
+float DISTMAXFIELD;
 
 void freeField(int ** field){
     for(int i = 0; i < SIZEMAP; i++){
@@ -40,72 +39,114 @@ void printField(int ** field){
     printf("-\n");
 }
 
+void fillMatrixFromPops(int ** field, Populations * pops){
+    for(int k = 0; k < 3; k++){
+        for(int i = 0; i < IndividualPerPopulation; i++){
+            int x = pops->pops[k].individuals[i].x;
+            int y = pops->pops[k].individuals[i].y;
+            field[x][y] = pops->pops[k].species;
+        }
+    }
+}
+
 // param select : 0 = prey, 1 = predator, 2 = ally
 void nearestPPA(Individual * indiv, Population * popPPA, int select){
     int iMin = 0;
-    float distMin = sqrt(pow(indiv->x - popPPA->individuals[0].x, 2) + pow(indiv->y - popPPA->individuals[0].y, 2));
+    float distMin = DISTMAXFIELD;
 
-    for(int i = 1; i < IndividualPerPopulation; i++){
-        float distTmp = sqrt(pow(indiv->x - popPPA->individuals[i].x, 2) + pow(indiv->y - popPPA->individuals[i].y, 2));
-        if(distTmp < distMin){
-            iMin = i;
-            distMin = distTmp;
+    for(int i = 0; i < IndividualPerPopulation; i++){
+        if(select != 2 || indiv->x != popPPA->individuals[i].x || indiv->y != popPPA->individuals[i].y) {
+            float distTmp = sqrt(
+                    pow(indiv->x - popPPA->individuals[i].x, 2) + pow(indiv->y - popPPA->individuals[i].y, 2));
+            if (distTmp < distMin) {
+                iMin = i;
+                distMin = distTmp;
+            }
         }
     }
 
     int xDiff = indiv->x - popPPA->individuals[iMin].x,
     yDiff = indiv->y - popPPA->individuals[iMin].y;
 
-    if (xDiff + yDiff > 0) {
-        if (xDiff - yDiff >= 0) indiv->status.infos[select].dir = W;
+    int DiffPlus = xDiff + yDiff,
+    DiffMinus = xDiff - yDiff;
+    if (DiffPlus > 0) {
+
+        if (DiffMinus >= 0) indiv->status.infos[select].dir = W;
         else indiv->status.infos[select].dir = N;
-    } else if (xDiff + yDiff < 0) {
-        if (xDiff - yDiff > 0) indiv->status.infos[select].dir = S;
+
+    } else if (DiffPlus < 0) {
+
+        if (DiffMinus > 0) indiv->status.infos[select].dir = S;
         else indiv->status.infos[select].dir = E;
+
     } else {
-        if (xDiff - yDiff > 0) indiv->status.infos[select].dir = S;
-        else if (xDiff - yDiff < 0) indiv->status.infos[select].dir = N;
+
+        if (DiffMinus > 0) indiv->status.infos[select].dir = S;
+        else if (DiffMinus < 0) indiv->status.infos[select].dir = N;
         else indiv->status.infos[select].dir = JOKER;
     }
 
-    if(distMin < PERCENTNEAR * sqrt(2) * SIZEMAP) indiv->status.infos[select].dist = NEAR;
-    else if(distMin < PERCENTMEDIUM * sqrt(2) * SIZEMAP) indiv->status.infos[select].dist = MEDIUM;
+    if(distMin < PERCENTNEAR * DISTMAXFIELD) indiv->status.infos[select].dist = NEAR;
+    else if(distMin < PERCENTMEDIUM * DISTMAXFIELD) indiv->status.infos[select].dist = MEDIUM;
     else indiv->status.infos[select].dist = AWAY;
 }
 
 void fillStatusPops(Populations * pops){
     for(int k = 0; k < 3; k++){
         for(int i = 0; i < IndividualPerPopulation; i++){
-            nearestPPA(&pops[k]->individuals[i], &pops[(k+1)%3], 0);
-            nearestPPA(&pops[k]->individuals[i], &pops[(k+2)%3], 1);
-            nearestPPA(&pops[k]->individuals[i], &pops[k], 2);
+            nearestPPA(&pops->pops[k].individuals[i], &pops->pops[(k+1)%3], 0);
+            nearestPPA(&pops->pops[k].individuals[i], &pops->pops[(k+2)%3], 1);
+            nearestPPA(&pops->pops[k].individuals[i], &pops->pops[k], 2);
         }
     }
 }
 
 int main(){
     int ** field = createField();
+    DISTMAXFIELD = sqrt(2) * SIZEMAX;
 
-    Population popPrey = {.individuals = {
-            {.x = 0, .y = 0},
+    Population popRed = {.individuals = {
             {.x = 0, .y = SIZEMAP - 1},
-            {.x = SIZEMAP - 1, .y = 0},
-            {.x = 4, .y = 13},
-            {.x = 12, .y = 5},
-            }, NULL, GREEN};
+            {.x = 2, .y = SIZEMAP - 1},
+            {.x = 4, .y = SIZEMAP - 1},
+            {.x = 1, .y = SIZEMAP - 2},
+            {.x = 6, .y = SIZEMAP - 1},
+            }, NULL, RED};
 
-    field[0][0] = 2;
-    field[0][SIZEMAP - 1] = 2;
-    field[SIZEMAP - 1][0] = 2;
-    field[4][13] = 2;
-    field[12][5] = 2;
+    Population popGreen = {.individuals = {
+            {.x = SIZEMAP - 1, .y = SIZEMAP - 1},
+            {.x = SIZEMAP - 3, .y = SIZEMAP - 1},
+            {.x = SIZEMAP - 5, .y = SIZEMAP - 1},
+            {.x = SIZEMAP - 2, .y = SIZEMAP - 2},
+            {.x = SIZEMAP - 4, .y = SIZEMAP - 2},
+    }, NULL, GREEN};
 
-    Individual indiv = {.x = 5, .y = 0};
-    field[5][0] = 1;
+    Population popBlue = {.individuals = {
+            {.x = 10, .y = 0},
+            {.x = 10, .y = 1},
+            {.x = 12, .y = 1},
+            {.x = 9, .y = 2},
+            {.x = 11, .y = 2},
+    }, NULL, BLUE};
 
-    printf("%d, %d\n", indiv.status.prey.dir, indiv.status.prey.dist);
-    nearestPPA(&indiv, popPrey, 0);
-    printf("%d, %d\n", indiv.status.prey.dir, indiv.status.prey.dist);
+    Populations pops = {{popRed, popGreen, popBlue}};
+
+    fillMatrixFromPops(field, &pops);
+
+    fillStatusPops(&pops);
+    printf("red 5 : [%d, _, %d, _, %d, _]\n", pops.r.individuals[4].status.prey.dir,
+           pops.r.individuals[4].status.predator.dir,
+           pops.r.individuals[4].status.ally.dir
+           );
+    printf("green 1 : [%d, _, %d, _, %d, _]\n", pops.g.individuals[0].status.prey.dir,
+           pops.g.individuals[0].status.predator.dir,
+           pops.g.individuals[0].status.ally.dir
+    );
+    printf("blue 1 : [%d, _, %d, _, %d, _]\n", pops.b.individuals[0].status.prey.dir,
+           pops.b.individuals[0].status.predator.dir,
+           pops.b.individuals[0].status.ally.dir
+    );
 
     printField(field);
     freeField(field);
