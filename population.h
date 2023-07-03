4b2d9332177fe0504038a2052af6ab20c0d40de4
MissingNo42 <romainklodzinski@live.com>
2023-07-03 12:01:20 +0200

    create pop

---
+++ population.h
@@ -0,0 +1,29 @@
+//
+// Created by Romain on 03/07/2023.
+//
+
+#ifndef ZZ1GAME_POPULATION_H
+#define ZZ1GAME_POPULATION_H
+
+#include "rules.h"
+
+#define IndividualPerPopulation 5
+
+typedef enum {
+	RED = 1,
+	GREEN = 2,
+	BLUE = 3,
+} Species;
+
+typedef struct {
+	Rule status;
+	int x, y, action;
+	Species species;
+} Individual;
+
+typedef struct {
+	Individual individuals[IndividualPerPopulation];
+	Brain * brain;
+} Population;
+
+#endif //ZZ1GAME_POPULATION_H
