11102f5b1c519254efc5ccdfdf101714ad5e55e0
Vurremt <72647204+Vurremt@users.noreply.github.com>
2023-07-04 18:36:37 +0200

    Changements

--- main.c
+++ main.c
@@ -52,7 +52,7 @@ int main(int argc, char ** argv) {
     srand(seed);
     printf("seed = %d\n", seed);
 	
-	/* Initialisation de la SDL  + gestion de l'échec possible */
+	/* Initialisation de la SDL + gestion de l'échec possible */
 	if (SDL_Init(SDL_INIT_VIDEO) != 0) {
 		SDL_Log("Error : SDL initialisation - %s\n", SDL_GetError()); // l'initialisation de la SDL a échoué
 		exit(EXIT_FAILURE);
