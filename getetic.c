19ef28fb2f89e3f07c5a7e7e310a9de0691520e1
anleu <anthony.leu@etu.isima.fr>
2023-07-06 10:55:57 +0200

    genetic file

--- getetic.c
+++ getetic.c
@@ -2,4 +2,30 @@
 // Created by lanth on 06/07/2023.
 //
 
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/stat.h>
+#include <dirent.h>
+#include <string.h>
+#include <math.h>
+#include <time.h>
+#include <threads.h>
+
+#include "population.h"
+#include "rules.h"
+#include "field.h"
 #include "getetic.h"
+
+void tournament(Brains  * brains){
+
+}
+
+void change_brains_order(Brains * brains){
+    for (int k = NB_BRAINS_CANDIDATE ; k>0; k--){
+        int i = rand() % k;
+        Brain * tmp = brains->brain[k-1];
+        brains->brain[k-1] = brains->brain[i];
+        brains->brain[i] = tmp;
+    }
+}
+
--- getetic.h
+++ getetic.h
@@ -5,4 +5,10 @@
 #ifndef ZZ1GAME_GETETIC_H
 #define ZZ1GAME_GETETIC_H
 
+#define NB_BRAINS_CANDIDATE 80
+
+
+
+
+
 #endif //ZZ1GAME_GETETIC_H
